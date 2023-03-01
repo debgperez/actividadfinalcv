8862c80407c677206f4a64793e9bb1032ad57f69
debgperez <debgperez@gmail.com>
2023-03-01 19:19:12 -0300

    Actividad final CV

---
+++ CVPinzón/funcion.js
@@ -0,0 +1,9 @@
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Ref: Armando Mendoza Tel: 1158698596 mail: amendoza@mail.com";
+}
+
+document.getElementById("boton2").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo2").innerHTML = "Ref: José Manuel Becerra Tel: 1158698597 mail: jmbecerra@mail.com";
+}
